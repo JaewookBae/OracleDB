99ea845b094e31416c10cc04207767a4d2c121a7
JaewookBae <cook_jw@naver.com>
2021-07-12 09:02:50 +0900

    0712

--- FinalProject.sql
+++ FinalProject.sql
@@ -18,6 +18,7 @@ CREATE SEQUENCE FP_Member_seq;
 
 --test
 SELECT * FROM FP_Member;
+
 -- 회원 가입 코드
 INSERT INTO FP_Member VALUES(
 FP_Member_seq.nextval,
@@ -29,3 +30,46 @@ FP_Member_seq.nextval,
 '010-1234-1234',
 '123@123.com',
 SYSDATE);
+
+--Board 테이블
+DROP TABLE FP_Board;
+CREATE TABLE FP_Board(
+    board_no NUMBER,
+    member_no NUMBER,
+    board_title VARCHAR2(400),
+    board_content VARCHAR2(4000),
+    board_readcount NUMBER,
+    board_writedate DATE
+);
+
+DROP SEQUENCE FP_Board_seq;
+CREATE SEQUENCE FP_Board_seq;
+
+--글 작성 쿼리
+INSERT INTO FP_Board VALUES(
+    FP_Board_seq.nextval,
+    1,
+    '테스트 제목입니다.',
+    '테스트 내용입니다.',
+    0,
+    SYSDATE
+);
+
+SELECT * FROM FP_Board,FP_Member ORDER BY board_no DESC;
+COMMIT;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
